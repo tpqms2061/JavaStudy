aefd1a614b2f24019529a1c31172542faa968818
tpqms2061 <tpqms2061@naver.com>
2025-06-10 01:56:42 +0900

    인프런 자바 - 날짜와 시간 라이브러리
    - 날짜 시간 핵심 인터페이스의 특징
    - 날짜 시간 조회 및 조작

---
+++ src/time/ChangTimeWithMain.java
@@ -0,0 +1,36 @@
+package time;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+public class ChangTimeWithMain {
+
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.of(2018, 1, 1, 13, 30, 59);
+        System.out.println("dt = " + dt);
+
+        LocalDateTime changeDt1 = dt.with(ChronoField.YEAR, 2020);
+        System.out.println("changeDt1 = " + changeDt1);
+
+
+        LocalDateTime changedDt2 = dt.withYear(2020);
+        System.out.println("changedDt2 = " + changedDt2);
+
+        //TemporalAdjuster 사용
+        //다음주 금요일
+
+        LocalDateTime with1 = dt.with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
+        System.out.println("기준 날짜  = " + dt);
+        System.out.println("다음 금요일 = " + with1);
+
+        //이번달의 마지막 일요일
+        LocalDateTime with2 = dt.with(TemporalAdjusters.lastInMonth(DayOfWeek.SATURDAY));
+        System.out.println("같은 달의 마지막 일요일 = " + with2);
+
+    }
+
+
+}
